f1d96f4a8770f12f117eb707e7cff1aede0fc896
Simmo Saan <simmo.saan@gmail.com>
2016-01-05 17:23:15 +0200

    Single/dual logo support

--- highscore.js
+++ highscore.js
@@ -1,12 +1,28 @@
-var logos = ["swedbank.png", "nutilabor.jpg",
-            "nutilabor.jpg", "swedbank.png"];
+var logos = [
+    ["1_Robootika_logo_taustata.png", "2_ FIRSTLego_iconHorz_RGB.png"],
+    ["3_EMT_Elion_f.png", "4_SEB_k_rgb.png"],
+    ["5_nutilabor.png", "6_swedbank.png"],
+    ["Suur_1_HMN_valge.png"],
+    ["Suur_2_MEIE VILISTLANE MUUDAB MAAILMA_us.png"],
+    ["Suur_3_robomiku_final.png"]
+];
 var logoi = 0;
 
-function renderLogos()
-{
-    $("#leftlogo").attr("src", logos[logoi]);
-    $("#rightlogo").attr("src", logos[logoi + 1]);
-    logoi += 2;
+function renderLogos() {
+    $(".onelogo, .twologo", "#header").hide();
+
+    var logo = logos[logoi];
+    if (logo.length == 1) {
+        $(".onelogo", "#header").show();
+        $("#onelogo").attr("src", "logo/" + logo[0]);
+    }
+    else if (logo.length == 2) {
+        $(".twologo", "#header").show();
+        $("#leftlogo").attr("src", "logo/" + logo[0]);
+        $("#rightlogo").attr("src", "logo/" + logo[1]);
+    }
+
+    logoi++;
     logoi %= logos.length;
 }
 
@@ -29,4 +45,4 @@ $(function() {
 
     $("html").scrollTop(0);
     animateScroll();
-}); 
+});
