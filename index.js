b50d13cf28de12bb587dc163dc1256ed6138295b
Simmo Saan <simmo.saan@gmail.com>
2017-11-05 14:22:33 +0200

    hydrodynamics: import from https://github.com/texasdiaz/FLLTS2013/blob/84391edc70bf93482bb1fbfe617065e6210780e8/language/Data.xml

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function loadAnswers(answers) {
 }
 
 $(function() {
-    $.get("data/animalallies.xml", function(data) {
+    $.get("data/hydrodynamics.xml", function(data) {
         elements = parseData(data);
         buildElements();
         loadAnswers(JSON.parse($("#formanswers").val()));
